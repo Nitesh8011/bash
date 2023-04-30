b45c8c084cd62cb75a2b47bea51d93b6167175d7
Nitesh Kumar <66053526+Nitesh8011@users.noreply.github.com>
2023-04-30 21:11:13 +0530

    Create country.sh

---
+++ bash_newcountry/country.sh
@@ -0,0 +1,14 @@
+#Given a list of countries, each on a new line, your task is to read them into an array and then display the entire array, 
+with a space between each of the countries' names.
+
+
+#!/bin/bash
+
+i=0
+
+while read line
+do
+    a[i]=$line
+    i=$((i+1))
+done
+echo ${a[@]};
