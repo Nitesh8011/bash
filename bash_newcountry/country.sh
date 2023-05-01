7bebaecae6f4cd4d1687b1c3ff4ed826dd25dc72
Nitesh Kumar <66053526+Nitesh8011@users.noreply.github.com>
2023-05-01 07:40:19 +0530

    Update country.sh

--- bash_newcountry/country.sh
+++ bash_newcountry/country.sh
@@ -12,3 +12,19 @@ do
     i=$((i+1))
 done
 echo ${a[@]};
+
+-----------x---------------x
+
+#Given a list of countries, each on a new line, your task is to read them into an array.
+#Then slice the array and display only the elements lying between positions 3 and 7, both inclusive. Indexing starts from 0.
+
+#!/bin/bash
+
+i=0
+
+while read line 
+do 
+    a[i]=$line
+    i=$(($i+1))
+done
+echo ${a[@]:3:5}
