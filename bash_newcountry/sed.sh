811594bca6381a43b6c73b6ac1d40973329c173d
Nitesh Kumar <66053526+Nitesh8011@users.noreply.github.com>
2023-05-14 06:05:26 +0530

    Update sed.sh

--- bash_newcountry/sed.sh
+++ bash_newcountry/sed.sh
@@ -1,2 +1,6 @@
 $sed 's/unix/{&}/gi' geekfile.txt
 # & represent original word and g is for golabl and i for insentitive
+
+
+$ sed 's/^.............../**** **** **** /'
+# replcae first 15 character with *
